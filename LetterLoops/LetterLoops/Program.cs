2ff3bb8579652cab3d667347ba467293d9f823b9
Austin Casey <thepoast@gmail.com>
2019-08-31 09:39:58 -0500

    same

---
+++ LetterLoops/LetterLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = Console.ReadLine();
+            var printOut = "";
+            foreach (char letter in letters)
+            {
+                var charIndex = letters.IndexOf(letter) + 1;
+                for (var i = charIndex; i > 0; i--)
+                {
+                    char currLetter;
+                    if (i == charIndex)
+                    {
+                        currLetter = char.ToUpper(letter);
+                    }
+                    else
+                    {
+                        currLetter = letter;
+                    }
+                    printOut += currLetter;
+                }
+                if (charIndex < letters.Length)
+                {
+                    printOut += "-";
+                }
+            }
+            Console.WriteLine(printOut);
+        }
+    }
+}
---
+++ metalwolfchaosxd2/metalwolfchaosxd2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace metalwolfchaosxd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var nums = input.Split(",");
+
+            var extraNums = new List<int>();
+
+            foreach (string number in nums)
+            {
+                var currNum = Convert.ToInt32(number);
+                extraNums.Add(currNum);
+            }
+
+            foreach (int number in extraNums)
+            {
+                var lol = number.Count;
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
